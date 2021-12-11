e8f8cf406130e708c3eb1263b7c9662f70ec11d5
Makise-K <felnovert.lee@gmail.com>
2021-12-11 23:47:49 +0800

    Heroku First Deployment emarket-hustle-rest

---
+++ src/main/java/org/emarket/hustle/hustleemarketrest/service/CustomerDetailService.java
@@ -0,0 +1,19 @@
+package org.emarket.hustle.hustleemarketrest.service;
+
+import java.util.List;
+
+import org.emarket.hustle.hustleemarketrest.entity.CustomerDetail;
+
+public interface CustomerDetailService
+{
+	public List<CustomerDetail> getCustomerDetail();
+
+	public CustomerDetail getCustomerDetailById(int id);
+
+	public void saveCustomerDetail(CustomerDetail customerDetail);
+
+	public void deleteCustomerDetailById(int id);
+
+	public void deleteCustomerDetail(CustomerDetail customerDetail);
+
+}
